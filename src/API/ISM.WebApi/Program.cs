e465b74f5be1b1887959f12d416445624047929c
aminlotfi2003 <lotfiamin2003@gmail.com>
2025-11-27 19:19:10 +0330

    feat(webapi): add database seeding and centralize pipeline setup via ApplicationBuilder extensions

--- src/API/ISM.WebApi/Program.cs
+++ src/API/ISM.WebApi/Program.cs
@@ -1,10 +1,4 @@
-using Asp.Versioning.ApiExplorer;
-using ISM.Domain.Identity;
-using ISM.Infrastructure.Persistence.Contexts;
-using ISM.WebFramework.Extensions;
 using ISM.WebFramework.Extensions.DependencyInjection;
-using ISM.WebFramework.Middlewares;
-using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,24 +8,8 @@ builder.Services.AddServices(builder.Configuration);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-var provider = app.Services.GetRequiredService<IApiVersionDescriptionProvider>();
-app.UseSwaggerConfigured(provider);
+await app.SeedDatabaseAsync();
 
-app.UseMiddleware<GlobalExceptionHandlingMiddleware>();
-
-using (var scope = app.Services.CreateScope())
-{
-    var userManager = scope.ServiceProvider.GetRequiredService<UserManager<ApplicationUser>>();
-    var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<ApplicationRole>>();
-
-    await ApplicationDbContextSeed.SeedAdminUserAsync(userManager, roleManager);
-}
-
-app.UseHttpsRedirection();
-
-app.UseAuthentication();
-app.UseAuthorization();
-
-app.MapControllers();
+app.UseWebFrameworkPipeline();
 
 app.Run();
---
+++ src/API/ISM.WebFramework/Extensions/DependencyInjection/ApplicationBuilderExtensions.cs
@@ -0,0 +1,39 @@
+﻿using Asp.Versioning.ApiExplorer;
+using ISM.Domain.Identity;
+using ISM.Infrastructure.Persistence.Contexts;
+using ISM.WebFramework.Middlewares;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ISM.WebFramework.Extensions.DependencyInjection;
+
+public static class ApplicationBuilderExtensions
+{
+    public static WebApplication UseWebFrameworkPipeline(this WebApplication app)
+    {
+        var provider = app.Services.GetRequiredService<IApiVersionDescriptionProvider>();
+        app.UseSwaggerConfigured(provider);
+
+        app.UseMiddleware<GlobalExceptionHandlingMiddleware>();
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthentication();
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        return app;
+    }
+
+    public static async Task SeedDatabaseAsync(this IApplicationBuilder app)
+    {
+        using var scope = app.ApplicationServices.CreateScope();
+
+        var userManager = scope.ServiceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+        var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<ApplicationRole>>();
+
+        await ApplicationDbContextSeed.SeedAdminUserAsync(userManager, roleManager);
+    }
+}
